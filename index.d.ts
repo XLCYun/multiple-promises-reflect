d1085a104cd879a383176552acf37112873a47de
XLCYun <XLCYun@foxmail.com>
2020-03-01 10:38:08 +0800

    complete declaration

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,49 @@
 // Type definitions for [multiple-promises-reflect]
+import { PReflectResolved } from "./index.d"
 // Definitions by: [XLCYun@foxmail.com] <[https://github.com/XLCYun]>
 declare function reflectOne<ResultData = any, ThrowError = Error>(
   promise: Promise<ResultData>,
   index: number,
-  injectReflectIndexToPromiseItSelfAsWell
+  injectReflectIndexToPromiseItSelfAsWell: boolean
 ): MultiplePromisesReflect.PromiseReflectResult<ResultData>
 
 declare function reflect<ResultData = any>(
+  promise: Promise<ResultData>,
+  injectReflectIndexToPromiseItSelfAsWell?: boolean
+): MultiplePromisesReflect.PromiseReflectResult<ResultData>
+
+declare function reflect<ResultData = any>(
+  promise: Promise<ResultData>[],
+  injectReflectIndexToPromiseItSelfAsWell?: boolean
+): MultiplePromisesReflect.PromiseReflectResult<ResultData>[]
+
+declare function reflect<ResultData = any>(
   promise: Promise<ResultData> | Promise<ResultData>[],
-  injectReflectIndexToPromiseItSelfAsWell: boolean
+  injectReflectIndexToPromiseItSelfAsWell?: boolean
 ): MultiplePromisesReflect.PromiseReflectResult<ResultData> | MultiplePromisesReflect.PromiseReflectResult<ResultData>[]
 
-declare namespace MultiplePromisesReflect {
-  export type PromiseReflectStatus = "resolved" | "rejected"
-
-  export interface PromiseReflectResult<ResultData, ThrowError = Error> {
-    data?: ResultData
-    error: ThrowError
-    status: PromiseReflectStatus
-    index: number | undefined
-  }
+export function isPReflectResolved<T extends PromiseReflectResult<any, any>>(res: T): res is PReflectResolved {
+  return res.status === "resolved"
 }
+
+export function isPReflectRejected<T extends PromiseReflectResult<any, any>>(res: T): res is PReflectRejected {
+  return res.status === "rejected"
+}
+
+export interface PReflectResolved<ResultData = any> {
+  data: ResultData
+  status: "resolved"
+  index: number | undefined
+}
+
+export interface PReflectRejected<RejectedObject = Error> {
+  error: RejectedObject
+  status: "rejected"
+  index: number | undefined
+}
+
+export type PromiseReflectResult<ResultData = any, RejectedObject = Error> =
+  | PReflectResolved<ResultData>
+  | PReflectRejected<RejectedObject>
 
 export = reflect
